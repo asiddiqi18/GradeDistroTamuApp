9f70fc1be26cd982772fa0038b2019f3b19442dd
Ammar S <60483997+asiddiqi18@users.noreply.github.com>
2021-07-16 18:34:42 -0500

    switched from POST to GET

--- website/views.py
+++ website/views.py
@@ -17,74 +17,64 @@ def home():
 
 @views.route('/professors', methods=["GET", "POST"])
 def professor():
-    form = CourseForm()
     source = "professor"
-    if request.method == 'GET':
-        return render_template("home.html", grade_results=None, form=form, source=source)
-    if request.method == 'POST':
-        source = "professor"
-        professor = request.form.get('professor')
-        # professors = Grades.query.filter_by(instructor=professor).all()
-        professors = Grades.query.filter(Grades.instructor.like(professor + "%")).all()
-        return render_template("home.html", grade_results=professors, form=form, source=source)
+    professor = request.args.get('professor')
+    form = CourseForm(professor=professor)
+    professors = Grades.query.filter(Grades.instructor.like(professor + "%")).all()
+    return render_template("home.html", grade_results=professors, form=form, source=source)
 
 
 @views.route('/results', methods=["GET", "POST"])
 def result():
-    form = CourseForm()
     source = "course"
-    if request.method == 'GET':
-        return render_template("home.html", grade_results=None, form=form, source=source)
-    if request.method == 'POST':
-        source = "course"
-        college = request.form.get('college')
-        semester = request.form.get('semester')
-        year = request.form.get('year')
+    college = request.args.get('college')
+    semester = request.args.get('semester')
+    year = request.args.get('year')
+    form = CourseForm(college=college, semester=semester, year=year)
 
+    print(f"{college}, {semester}, {year}")
 
-        print(f"{college}, {semester}, {year}")
+    grades = Grades.query.filter_by(college=college, semester=semester, year=year).all()
 
-        grades = Grades.query.filter_by(college=college, semester=semester, year=year).all()
+    if (grades):
+        print("Records in database... retrieving from database...")
+    else:
+        print("Records not in database... retrieving from PDF...")
+        pdf_json = PdfToJson(college, year, semester)
+        results = pdf_json.get_list_of_lists()
+        grades = []
+        for result in results:
+            new_grade = Grades(
+                college=college,
+                year=year,
+                semester=semester,
+                department=result[0],
+                course=result[1],
+                section=result[2],
+                amount_A=result[3],
+                percent_A=result[4],
+                amount_B=result[5],
+                percent_B=result[6],
+                amount_C=result[7],
+                percent_C=result[8],
+                amount_D=result[9],
+                percent_D=result[10],
+                amount_F=result[11],
+                percent_F=result[12],
+                total_A_F=result[13],
+                gpa=result[14],
+                other_I=result[15],
+                other_S=result[16],
+                other_U=result[17],
+                other_Q=result[18],
+                other_X=result[19],
+                other_total=result[20],
+                instructor=result[21]
+            )
 
-        if (grades):
-            print("Records in database... retrieving from database...")
-        else:
-            print("Records not in database... retrieving from PDF...")
-            pdf_json = PdfToJson(college, year, semester)
-            results = pdf_json.get_list_of_lists()
-            grades = []
-            for result in results:
-                new_grade = Grades(
-                    college=college,
-                    year=year,
-                    semester=semester,
-                    department=result[0],
-                    course=result[1],
-                    section=result[2],
-                    amount_A=result[3],
-                    percent_A=result[4],
-                    amount_B=result[5],
-                    percent_B=result[6],
-                    amount_C=result[7],
-                    percent_C=result[8],
-                    amount_D=result[9],
-                    percent_D=result[10],
-                    amount_F=result[11],
-                    percent_F=result[12],
-                    total_A_F=result[13],
-                    gpa=result[14],
-                    other_I=result[15],
-                    other_S=result[16],
-                    other_U=result[17],
-                    other_Q=result[18],
-                    other_X=result[19],
-                    other_total=result[20],
-                    instructor=result[21]
-                )
+            grades.append(new_grade)
 
-                grades.append(new_grade)
+        db.session.add_all(grades)
+        db.session.commit()
 
-            db.session.add_all(grades)
-            db.session.commit()
-
-        return render_template("home.html", grade_results=grades, form=form, source=source)
+    return render_template("home.html", grade_results=grades, form=form, source=source)
