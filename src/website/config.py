6575d9623f9a0777ce6b181b99eef725de1b6244
Ammar S <60483997+asiddiqi18@users.noreply.github.com>
2022-03-10 22:49:50 -0600

    changed to production

--- src/website/config.py
+++ src/website/config.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
-FLASK_ENV = 'development'
-TESTING = True
+FLASK_ENV = 'production'
+TESTING = False
+DEBUG = False
 SECRET_KEY = environ.get('SECRET_KEY')
 STATIC_FOLDER = './static'
 TEMPLATES_FOLDER = './templates'
