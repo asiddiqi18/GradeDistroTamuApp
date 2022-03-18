c6f820cfb3fcc13dd4ae4fe67eb628b8df943113
Ammar S <60483997+asiddiqi18@users.noreply.github.com>
2022-03-17 19:52:59 -0500

    changed to production

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "grades.db"
 def create_app(unit_testing=False):
     app = Flask(__name__)
 
-    app.config.from_object('src.website.config.DevConfig')
+    app.config.from_object('src.website.config.ProdConfig')
     db.init_app(app)
 
     if not unit_testing:
